9b63c8c4f4d0b3b9ba88725b5a00779933853e1b
digital-programmer <mohandibya123@gmail.com>
2021-06-19 21:03:40 +0530

    API release

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  let weatherData = { weather: null };
+  let message = null;
+  return res.status(200).render("index", weatherData);
+});
+
+app.post("/", (req, res) => {
+  let city_name = req.body.city;
+  const API_Key = "5a77d8f5eb2ded236463928bb6696752";
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=${API_Key}`;
+
+  request(url, (error, response, body) => {
+    const weatherJSON = JSON.parse(body);
+    // console.log(weatherJSON);
+
+    if (weatherJSON.cod == "200") {
+      let weather = {
+        city: city_name,
+        temperature: Math.round(weatherJSON.main.temp),
+        description: weatherJSON.weather[0].description,
+        windspeed: weatherJSON.wind.speed,
+        humidity: weatherJSON.main.humidity,
+        icon: weatherJSON.weather[0].icon,
+        pressure: weatherJSON.main.pressure,
+        success: true,
+      };
+
+      let weatherData = { weather: weather };
+      return res.render("index", weatherData);
+    } else if (weatherJSON.cod == "401") {
+      let weather = {
+        city: city_name,
+        success: false,
+      };
+      let weatherData = { weather: weather };
+      return res.render("index", weatherData);
+    } else if (weatherJSON.cod == "404") {
+      let weather = {
+        city: city_name,
+        success: false,
+      };
+      let weatherData = { weather: weather };
+      return res.render("index", weatherData);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("App listening on port 3000!");
+});
